ca2ac8a9f5df13f78ba411e559bcd8f6f5fd5891
Pwint Phoo Wai <phuephue1125@gmail.com>
2026-02-23 10:25:53 +0630

    fix db

---
+++ src/Features/auth/user.interface.ts
@@ -0,0 +1,14 @@
+import { Profile, User } from "@prisma/client";
+import { CreateUser, ProfileResponse, UpdateProfile, UserCreateResponse, UserLogin } from "./types/user";
+import { UpdateProfileBody } from "./user.repository";
+
+export interface IAuthRepository {
+    findByEmailOrPhone(emailOrPhone: string): Promise<UserLogin | null>;
+    createUser(userData: CreateUser): Promise<UserCreateResponse>;
+    loginUser(emailOrPhone: string, password: string): Promise<UserLogin | any>;
+    getProfile(userId: string): Promise<any>;
+    updateProfile(
+        updateDataInput: UpdateProfileBody,
+        userId: string
+    ): Promise<any>;
+}
